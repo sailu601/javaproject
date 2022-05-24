fd960f15f0ed76fb91b66ae756c5f39d9141f2fd
saila <saila@DESKTOP-12QMHCJ>
2022-05-24 16:56:09 -0400

    first project

---
+++ src/Devops/firstclass.java
@@ -0,0 +1,13 @@
+package Devops;
+
+public class firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//a=40
+	//Welcome to my project 
+		
+		//this is my first project
+	}
+
+}
